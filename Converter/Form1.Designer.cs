5c60171b671457c81bbcae687d1ad5977dbfbb1a
meethu51 <85707157+meethu51@users.noreply.github.com>
2022-07-23 19:50:46 +0800

    SquareAndSquareRootConverter

---
+++ Converter/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿
+namespace Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.txtResults1 = new System.Windows.Forms.TextBox();
+            this.lblSquare = new System.Windows.Forms.Label();
+            this.lblEql = new System.Windows.Forms.Label();
+            this.btnConvert = new System.Windows.Forms.Button();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtResults2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(86, 60);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(100, 22);
+            this.txtNum1.TabIndex = 0;
+            // 
+            // txtResults1
+            // 
+            this.txtResults1.Location = new System.Drawing.Point(303, 61);
+            this.txtResults1.Name = "txtResults1";
+            this.txtResults1.Size = new System.Drawing.Size(100, 22);
+            this.txtResults1.TabIndex = 1;
+            // 
+            // lblSquare
+            // 
+            this.lblSquare.AutoSize = true;
+            this.lblSquare.Location = new System.Drawing.Point(12, 64);
+            this.lblSquare.Name = "lblSquare";
+            this.lblSquare.Size = new System.Drawing.Size(70, 17);
+            this.lblSquare.TabIndex = 2;
+            this.lblSquare.Text = "Square of";
+            // 
+            // lblEql
+            // 
+            this.lblEql.AutoSize = true;
+            this.lblEql.Location = new System.Drawing.Point(231, 63);
+            this.lblEql.Name = "lblEql";
+            this.lblEql.Size = new System.Drawing.Size(16, 17);
+            this.lblEql.TabIndex = 3;
+            this.lblEql.Text = "=";
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(202, 146);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 4;
+            this.btnConvert.Text = "Convert";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(86, 106);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(100, 22);
+            this.txtNum2.TabIndex = 5;
+            // 
+            // txtResults2
+            // 
+            this.txtResults2.Location = new System.Drawing.Point(303, 106);
+            this.txtResults2.Name = "txtResults2";
+            this.txtResults2.Size = new System.Drawing.Size(100, 22);
+            this.txtResults2.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(10, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 38);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Square Root Of";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(231, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(16, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "=";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(33, 251);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(263, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Done by: Bhuvan Shrivastava(203652R)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 295);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtResults2);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.btnConvert);
+            this.Controls.Add(this.lblEql);
+            this.Controls.Add(this.lblSquare);
+            this.Controls.Add(this.txtResults1);
+            this.Controls.Add(this.txtNum1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.TextBox txtResults1;
+        private System.Windows.Forms.Label lblSquare;
+        private System.Windows.Forms.Label lblEql;
+        private System.Windows.Forms.Button btnConvert;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtResults2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Converter/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtNum1.Text = "0";
+            txtResults1.Text = "0";
+
+        }
+        
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            float num1, results1;
+            num1 = float.Parse(txtNum1.Text);
+            results1 = num1 * num1;
+            txtResults1.Text = results1.ToString();
+
+            double num2, results2;
+            num2 = double.Parse(txtNum2.Text);
+            results2 = Math.Sqrt(num2);
+            txtResults2.Text = results2.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
